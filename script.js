d0bf6ac663c0a46dd184e2d77dd7ae4e3363b20d
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-06 22:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function getFormvalue() {
-    //Write your code here
-	var firstName = document.getElementById("fname").value;
-    var lastName = document.getElementById("lname").value;
-	var fullName = firstName + " " + lastName;
-    window.alert(fullName);
+function getFormvalue(event) {
+
+//Write your code here
+
+event.preventDefault();
+
+var firstName = document.getElementById(“fname”).value;
+
+var lastName = document.getElementById(“lname”).value;
+
+var fullName = firstName + " " + lastName;
+
+window.alert(fullName);
 
 }
+
+getFormvalue();
