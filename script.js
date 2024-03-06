a41dbbf3f1bf89dd5882866dd27a6cd87041e966
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-06 21:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var firstName = document.getElementById("fname").value;
+    var lastName = document.getElementById("lname").value;
+	var fullName = firstName + " " + lastName;
+    window.alert(fullName);
 
 }
